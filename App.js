d4e4574140fa34539991bd1526421c900ecd360a
Callum Song <ghksong@gmail.com>
2018-06-24 17:47:18 +0100

    Add new code to change state and add tests

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+import image from './logo-blue-side.png';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { text: '' };
+  }
+
+  handleChange = (text) => {
+    const name = text.length ? ` ${text}.` : '';
+    this.setState({
+      text: name,
+    });
+  }
+
   render() {
+    const styles = StyleSheet.create({
+      container: {
+        backgroundColor: '#fff',
+      },
+      image: {
+        width: 150,
+        height: 100,
+        resizeMode: 'contain',
+      },
+    });
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <Image
+            source={image}
+            style={styles.image}
+          />
+        </View>
+        <View>
+          <Text>Hello, my name is{this.state.text}</Text>
+          <TextInput
+            onChangeText={this.handleChange}
+          />
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- App.test.js
+++ App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-import renderer from 'react-test-renderer';
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+describe('App', () => {
+  const wrapper = shallow(<App />);
+
+  it('should have one Image', () => {
+    expect(wrapper.find('Image').length).toBe(1);
+  });
+
+  it('should have no name and no full stop if there is no value', () => {
+    expect(wrapper.find('Text').length).toBe(1);
+    expect(wrapper.find('Text').first().dive().text())
+      .toBe('Hello, my name is');
+  });
+
+  it('should be able to handle change of text', () => {
+    wrapper.instance().handleChange('Bob');
+    wrapper.setState(wrapper.instance().state);
+    expect(wrapper.find('Text').first().dive().text())
+      .toBe('Hello, my name is Bob.');
+  });
 });
